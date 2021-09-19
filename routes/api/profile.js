9fdfaf4515de0dd7325d45ead2122c27fb060ac1
Santosh <santosh@thinkdeeply.com>
2021-09-19 17:45:45 +0530

    get github user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 
 const auth = require('../../middleware/auth');
@@ -207,6 +209,27 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     console.error(error.message);
     res.status(500).send('Server error while removing education');
   }
+});
+
+// github get
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get(
+        'githubClientSecret'
+      )}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ mes: 'No Github profile found' });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {}
 });
 
 module.exports = router;
