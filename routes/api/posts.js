61d1a8edd8ce129357fada4ec5636c0f7280096d
Santosh Pasupunuri <skpadmin@hyperiongrp.com>
2021-10-01 23:13:12 +0530

    adding post model and post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route get api/posts
-// @desc Test route
-// @access Public
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Posts route'));
+// @route POST api/posts
+// @desc create a post
+// @access Private
+
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      res.status(401).json({ msg: 'Invalid token' });
+    }
+  }
+);
 
 module.exports = router;
